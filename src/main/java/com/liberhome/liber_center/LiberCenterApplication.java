1ad82d2920e614b42411bcc375dd36ccd075a87f
liberhome <liberhome@liberhomedeMacBook-Air-3.local>
2023-01-08 12:24:43 +0800

    liber center init

---
+++ src/main/java/com/liberhome/liber_center/LiberCenterApplication.java
@@ -0,0 +1,13 @@
+package com.liberhome.liber_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiberCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liberhome/liber_center/LiberCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liberhome.liber_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
